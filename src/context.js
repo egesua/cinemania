ae498067161bcc45be8eaa65b5816aac0c3ae8b3
egesua <egesu06@gmail.com>
2024-04-15 12:44:21 +0300

    fetching data from omdb database.

--- src/context.js
+++ src/context.js
@@ -1,15 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+
+const API_URL = `http://www.omdbapi.com/?apikey=727bbdc1&s=titanic`;
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  useEffect(() => {
+    const getMovies = async (url) => {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getMovies(API_URL);
+  }, []);
+
   return <AppContext.Provider value="egesua">{children}</AppContext.Provider>;
 };
 
-
 const useGlobalContext = () => {
   return useContext(AppContext);
-}
-
+};
 
 export { AppContext, AppProvider, useGlobalContext };
