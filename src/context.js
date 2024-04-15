40c51512c28cfb5d8d20473ea4be7e72e251ebf9
egesua <egesu06@gmail.com>
2024-04-15 21:59:21 +0300

    context reduction

--- src/context.js
+++ src/context.js
@@ -1,32 +1,17 @@
 import React, { useContext, useState } from "react";
-
-const API_URL = `http://www.omdbapi.com/?apikey=727bbdc1&s=titanic`;
+import useFetch from "./useFetch";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movie, setMovie] = useState([]);
-  const [isError, setIsError] = useState({ show: "false", msg: "" });
-  const [query, setQuery] = useState("titanic");
+  const [query, setQuery] = useState("hacker");
+  const { isLoading, isError, movie } = useFetch(`&s=${query}`);
 
-  const getMovies = async (url) => {
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data);
-      if (data.Response === "True") {
-        setIsLoading(false);
-        setMovie(data.Search);
-      } else {
-        setIsError({ show: true, msg: data.error });
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  getMovies(API_URL);
-  return <AppContext.Provider value={{isLoading, isError, movie}}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ query, movie, setQuery, isLoading, isError }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 const useGlobalContext = () => {
