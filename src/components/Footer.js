ec62adca89adef4515ec9c5de998d61cf16d8b3a
egesua <egesu06@gmail.com>
2024-04-17 21:42:12 +0300

    creating the footer with only boilerplate.

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "./Search";
+
+const Footer = () => {
+  return (
+    <div className="container">
+      <Search />
+    </div>
+  );
+};
+
+export default Footer;
